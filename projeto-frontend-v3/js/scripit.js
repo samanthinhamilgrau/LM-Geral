eee3e8be5d1526e08dfd0ef6a8728ba60bdcaf1e
samanthinhamilgrau <samanthapcaetano809@gmail.com>
2023-05-23 11:13:55 -0300

    LM- GERAL-AULAS

---
+++ projeto-frontend-v3/js/scripit.js
@@ -0,0 +1,15 @@
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = ()=> {
+    // alert("CLICOU!");
+
+    if(darkmode.classList.contains('bx-moon')){
+        //alert("MODO ESCURO ATIVADO!");
+        darkmode.classList.replace('bx-moon', 'bx-sun');
+        document.body.classList.add('color');
+    }else{
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('color');
+    }
+}
+        
